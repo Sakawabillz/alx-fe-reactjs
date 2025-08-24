958e675b98949cfa6d9cb43f99ea3bedd8ca44e5
Sakawabillz <paschalflexy10@gmail.com>
2025-08-24 09:33:14 -0700

    fix: move BrowserRouter to App.jsx and use /blog/:id dynamic route; keep ProtectedRoute

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,38 +1,41 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Blog from "./pages/Blog.jsx";
-import BlogPost from "./pages/BlogPost.jsx"; // dynamic route: /blog/:postId
+import BlogPost from "./pages/BlogPost.jsx"; // dynamic route target
 import Profile from "./components/Profile.jsx";
 import Login from "./pages/Login.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* Public routes */}
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="blog" element={<Blog />} />
-        <Route path="blog/:postId" element={<BlogPost />} /> {/* Dynamic routing implemented */}
-        <Route path="login" element={<Login />} />
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="blog/:id" element={<BlogPost />} /> {/* Dynamic routing implemented */}
+            <Route path="login" element={<Login />} />
 
-        {/* Protected parent route; children are handled inside Profile.jsx */}
-        <Route
-          path="profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+            {/* Protected route implemented */}
+            <Route
+              path="profile/*"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
 
-        {/* 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
